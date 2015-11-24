753a0f4ff91f7038e84c46ad095aa233b2aba4bb
Dmytro Zaichenko <dim2002@ukr.net>
2015-11-24 15:27:46 +0200

    comparator from teacher

--- src/hashCode/Person.java
+++ src/hashCode/Person.java
@@ -8,8 +8,30 @@ public class Person {
     private int age;
     private long salary;
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        salary = 1316;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Person){
+            Person p = (Person) obj;
+            if (name != null && name.equals(p.name)
+                && p.age == age  && p.salary == salary){
+                return true;
+            }
+        }
+        return false;
+    }
 
-
-
+    @Override
+    public int hashCode() {
+        int result = 23;
+        result = 37 * result + name.hashCode();
+        result = 37 * result + age;
+        result = 37 * result + new Long(salary).hashCode();
+        return result;
+    }
 }
