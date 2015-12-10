65d63271e2c9c68c7913f7334a4d04b8d6aca24f
Dmytro Zaichenko <dim2002@ukr.net>
2015-12-10 13:50:37 +0200

    with remove error from Node(SimpleLinkedList)

--- src/node/Demo.java
+++ src/node/Demo.java
@@ -4,54 +4,6 @@ public class Demo {
 
     public static void main(String[] args){
 
-        System.out.print(sll.getSize()+" ");
-//
-//        String mark = new String("mark");
-//
-//        sll.addLast(mark);
-//        sll.addLast("test 0");
-//        System.out.print(sll.getSize() + " ");
-//        sll.printList();
-//
-//        String middle = new String("middle");
-//        sll.addAfter(middle, mark);
-//        System.out.print(sll.getSize() + " ");
-//        sll.printList();
-//
-//        sll.addAfter("middle (-1)", mark);
-//        System.out.print(sll.getSize() + " ");
-//        sll.printList();
-//
-//        System.out.println();
-//        System.out.println("test addAfter, lost size == 1");
-//        System.out.println();
-//
-//        sll = new SimpleLinkedList();
-//        sll.addLast(mark);
-//        System.out.println(sll.getSize() + "");
-//        sll.printList();
-//
-//        sll.addAfter("last", mark);
-//        System.out.print(sll.getSize() + " ");
-//        sll.printList();
-//
-//        System.out.println();
-//        System.out.println("test addAfter, no prev element exception");
-//        System.out.println();
-//
-//        sll = new SimpleLinkedList();
-//        sll.addLast("test");
-//        System.out.print(sll.getSize() + " ");
-//        sll.printList();
-//        System.out.println();
-//
-//        sll.addAfter("last", mark);
-//        System.out.print(sll.getSize() + " ");
-//        sll.printList();
-
-        System.err.println("test iterator");
-        System.out.println();
-
         SimpleLinkedList sll = new SimpleLinkedList();
         sll.addFirst("1. First");
         sll.addFirst("2. Second");
